5e497aeef211af64098ff4d79aebbf4950f5f5fa
Khachatryan Marsel <marselvkhachatryan@gmail.com>
2023-05-10 08:28:01 +0300

    Исправил webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,33 @@
+const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
-    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader',],
+            },
+            
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCSSExtractPlugin()
+    ]
 }
